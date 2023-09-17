4d758ee6c17a3d735400a56946294012b6b853a8
IcaroIyusuka <115050400+IcaroIyusuka@users.noreply.github.com>
2023-09-16 22:25:40 -0300

    Update somaTestJUnit.java

--- CalculadoraJUnit/src/calculadora/somaTestJUnit.java
+++ CalculadoraJUnit/src/calculadora/somaTestJUnit.java
@@ -21,17 +21,19 @@ public class somaTestJUnit {
 		
 		//calculo de dois valores sendo um deles igual a zero.
 		soma = calc.somar(3,0);
-		//o valor colocado é o resultado 
+		//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 3.
 		Assert.assertEquals(3, soma);
 		
 		
-		
+		//calculo de dois valores sendo eles igual a zero.
 		 soma = calc.somar(0,0);
+		//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 0.
 		 Assert.assertEquals(0, soma);
 		
 		
-		
+		//calculo de dois valores sendo um deles negativo.
 		 soma = calc.somar(3,-1);
+		//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 2.
 		 Assert.assertEquals(2, soma);
 		
 	}
