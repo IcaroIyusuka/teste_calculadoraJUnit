c4c2e5f138cc586c1f876147f9a4666693d0f50a
IcaroIyusuka <115050400+IcaroIyusuka@users.noreply.github.com>
2023-09-18 13:14:23 -0300

    Update somaTestJUnit.java2

--- CalculadoraJUnit/src/calculadora/somaTestJUnit.java
+++ CalculadoraJUnit/src/calculadora/somaTestJUnit.java
@@ -7,34 +7,34 @@ import junit.framework.Assert;
 
 public class somaTestJUnit {
 	
-	@SuppressWarnings("deprecation")
-	@Test
+	@SuppressWarnings("deprecation")//Para importa e deixar funcional o assert.
+	@Test//Para importar o JUnit pack, de maneira funcional.
 	public void Calculodesoma() {
-		//variavel da calculadora para simplificar.
-		calculadora calc = new calculadora();
+		
+		calculadora calc = new calculadora();//variavel da calculadora para simplificar.
+		
 		//efetuar o calculo dos numeros inteiros dentro dos colchetes.
-		//calculo de dois valores.
-		int soma = calc.somar(3,7);
-		//o valor colocado junto do comando soma, é para validar o valor, sendo assim o resultado do calculo acima é 10.
-		Assert.assertEquals(10, soma);
+		int soma = calc.somar(3,7);//calculo de dois valores.
+		
+		Assert.assertEquals(10, soma);//o valor colocado junto do comando soma, é para validar o valor, sendo assim o resultado do calculo acima é 10.
 		
 		
-		//calculo de dois valores sendo um deles igual a zero.
-		soma = calc.somar(3,0);
-		//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 3.
-		Assert.assertEquals(3, soma);
+		
+		soma = calc.somar(3,0);//calculo de dois valores sendo um deles igual a zero.
+		
+		Assert.assertEquals(3, soma);//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 3.
 		
 		
-		//calculo de dois valores sendo eles igual a zero.
-		 soma = calc.somar(0,0);
-		//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 0.
-		 Assert.assertEquals(0, soma);
+		
+		 soma = calc.somar(0,0);//calculo de dois valores sendo eles igual a zero.
+		
+		 Assert.assertEquals(0, soma);//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 0.
 		
 		
-		//calculo de dois valores sendo um deles negativo.
-		 soma = calc.somar(3,-1);
-		//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 2.
-		 Assert.assertEquals(2, soma);
+		
+		 soma = calc.somar(3,-1);//calculo de dois valores sendo um deles negativo.
+		
+		 Assert.assertEquals(2, soma);//o valor colocado é o resultado da soma dos dois valores sem assim o valor de 2.
 		
 	}
 
