1bd68d81322c5a6fb348e7d8d3d79e4ec31d53b0
IcaroIyusuka <115050400+IcaroIyusuka@users.noreply.github.com>
2023-09-18 13:30:57 -0300

    Update divisaoTestJUnit.java2

--- CalculadoraJUnit/src/calculadora/divisaoTestJUnit.java
+++ CalculadoraJUnit/src/calculadora/divisaoTestJUnit.java
@@ -1,33 +1,38 @@
 package calculadora;
-
+//importação da api para utilização do JUnit.
 import org.junit.jupiter.api.Test;
-
+//importação do framework para a funcionalidade do "assert".
 import junit.framework.Assert;
 
 public class divisaoTestJUnit {
 
 	
-	@SuppressWarnings("deprecation")
-	@Test
+	@SuppressWarnings("deprecation")//Para importa e deixar funcional o assert
+	
+	@Test//Para importar o JUnit pack, de maneira funcional.
 	public void calculodivisao() {
-		
+		//Simplificar a instacia da calculadora.
 		calculadora calc = new calculadora();
 		
+		//efetuar o calculo dos numeros inteiros dentro dos colchetes.
+		int div = calc.divisao(4, 2);//Calculando a divisao de dois valroes.
 		
-		int div = calc.divisao(4, 2);
-		Assert.assertEquals(2, div);
-		
-		
-		div = calc.divisao(1, 2);
-		Assert.assertEquals(0, div);
+		Assert.assertEquals(2, div);//o valor colocado junto do comando div, é para validar o valor, sendo assim o resultado do calculo acima é 2.
 		
 		
-		div = calc.divisao(2, 2);
-		Assert.assertEquals(1, div);
+		div = calc.divisao(1, 2);//Calculando a divisão de dois valores, sendo o dividendo menor que o divisor.
+		
+		Assert.assertEquals(0, div);//o valor colocado junto do comando div, é para validar o valor, sendo assim o resultado do calculo acima é 0.
 		
 		
-		div = calc.divisao(0, 2);
-		Assert.assertEquals(0 ,div);
+		div = calc.divisao(2, 2);//Calculando a divisão de dois valores iguais
+		
+		Assert.assertEquals(1, div);//o valor colocado junto do comando div, é para validar o valor, sendo assim o resultado do calculo acima é 1.
+		
+		
+		div = calc.divisao(0, 2);//Calculando a divisão de dois valores, sendo o dividendo igual a zero.
+		
+		Assert.assertEquals(0 ,div);//o valor colocado junto do comando div, é para validar o valor, sendo assim o resultado do calculo acima é 0.
 	}
 
 }
