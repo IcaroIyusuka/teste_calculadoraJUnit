636a185f8f9067656b144bd0f61861e5f6c6ef0e
IcaroIyusuka <115050400+IcaroIyusuka@users.noreply.github.com>
2023-09-18 13:31:06 -0300

    Update multiplicacaoTestJUnit.java2

--- CalculadoraJUnit/src/calculadora/multiplicacaoTestJUnit.java
+++ CalculadoraJUnit/src/calculadora/multiplicacaoTestJUnit.java
@@ -8,34 +8,34 @@ public class multiplicacaoTestJUnit {
 
 	
 	
-	@SuppressWarnings("deprecation")
-	@Test
+	@SuppressWarnings("deprecation")//Para importa e deixar funcional o assert.
+	@Test//Para importar o JUnit pack, de maneira funcional.
 	public void calculodemuliplicacao() {
-		//variavel da calculadora para simplificar.
+		//Simplificar a instacia da calculadora.
 		calculadora calc = new calculadora();
 		
 		//efetuar o calculo dos numeros inteiros dentro dos colchetes.
-		//calculo de dois valores.
-		int mult = calc.multiplicacao(8, 7);
-		//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 56.
-		Assert.assertEquals(56, mult);
 		
-		//calculo de dois valores sendo um deles igual a zero.
-		mult = calc.multiplicacao(9, 0);
-		//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 0.
-		Assert.assertEquals(0, mult);
+		int mult = calc.multiplicacao(8, 7);//calculo da multiplicação de dois valores.
+		
+		Assert.assertEquals(56, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 56.
+		
+		
+		mult = calc.multiplicacao(9, 0);//calculo da multiplicação de dois valores sendo um deles igual a zero.
+		
+		Assert.assertEquals(0, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 0.
 	
 		
-		//calculo de dois valores sendo um deles igual a 1.
-		mult = calc.multiplicacao(5, 1);
-		//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 10.
-		Assert.assertEquals(5, mult);
+		
+		mult = calc.multiplicacao(5, 1);//calculo da multiplicação de dois valores sendo um deles igual a 1.
+		
+		Assert.assertEquals(5, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 10.
 	
 		
-		//calculo de dois valores sendo um deles negativo.
-		mult = calc.multiplicacao(8, -7);
-		//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é -56.
-		Assert.assertEquals(-56, mult);
+		
+		mult = calc.multiplicacao(8, -7);//calculo da multiplicação de dois valores sendo um deles negativo.
+		
+		Assert.assertEquals(-56, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é -56.
 	}
 
 }
