53d09aca8dab0613be2f24f31a106172632f8726
IcaroIyusuka <115050400+IcaroIyusuka@users.noreply.github.com>
2023-09-26 09:44:35 -0300

    Update multiplicacaoTestJUnit.java2

--- CalculadoraJUnit/src/calculadora/multiplicacaoTestJUnit.java
+++ CalculadoraJUnit/src/calculadora/multiplicacaoTestJUnit.java
@@ -18,24 +18,29 @@ public class multiplicacaoTestJUnit {
 		
 		int mult = calc.multiplicacao(8, 7);//calculo da multiplicação de dois valores.
 		
-		Assert.assertEquals(56, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 56.
+		Assert.assertEquals(56, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo é 56.
 		
 		
 		mult = calc.multiplicacao(9, 0);//calculo da multiplicação de dois valores sendo um deles igual a zero.
 		
-		Assert.assertEquals(0, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 0.
+		Assert.assertEquals(0, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo é 0.
 	
 		
 		
 		mult = calc.multiplicacao(5, 1);//calculo da multiplicação de dois valores sendo um deles igual a 1.
 		
-		Assert.assertEquals(5, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é 10.
+		Assert.assertEquals(5, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo é 10.
 	
-		
 		
 		mult = calc.multiplicacao(8, -7);//calculo da multiplicação de dois valores sendo um deles negativo.
 		
-		Assert.assertEquals(-56, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo acima é -56.
+		Assert.assertEquals(-56, mult);//o valor colocado junto do comando mult, é para validar o valor, sendo assim o resultado do calculo é -56.
+
+		//O teste abaixo é para um valor invalido
+		mult = calc.multiplicacao(8, 7);//calculo da multiplicação de dois valores.
+		
+		Assert.assertEquals(54, mult);//o valor colocado junto do comando mult, é para validar o valor, mas esse valor esta invalido nessa execução.
+		
 	}
 
 }
