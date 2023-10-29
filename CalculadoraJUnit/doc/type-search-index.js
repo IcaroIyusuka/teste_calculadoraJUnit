ed12e8391d901cb7d01a15bf49ba4f039a6de0bb
IcaroIyusuka <115050400+IcaroIyusuka@users.noreply.github.com>
2023-10-29 18:06:50 -0300

    Teste2

---
+++ CalculadoraJUnit/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"calculadora","l":"Calculadora"},{"p":"calculadora","l":"calculadora_teste_geral"},{"p":"calculadora","l":"Teste_Divisao"},{"p":"calculadora","l":"Teste_Multiplicacao"},{"p":"calculadora","l":"Teste_soma"},{"p":"calculadora","l":"Teste_subtracao"}];updateSearchResults();
